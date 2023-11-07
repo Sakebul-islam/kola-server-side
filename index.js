e13e4417402e030e74a43a1c6dabcba131c30eda
Sakebul-islam <mdsakebul66@gmail.com>
2023-11-07 22:43:19 +0600

    filter item on the basis of queary filter item on DB and return back

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 // DB URI
-const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_KEY_DB}@cluster0.ltwp59m.mongodb.net/cleanCoDB?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.USER_ID_DB}:${process.env.USER_KEY_DB}@cluster0.ltwp59m.mongodb.net/cleanCoDB?retryWrites=true&w=majority`;
 
 // MongoDB Client Connection
 const client = new MongoClient(uri, {
@@ -29,12 +29,57 @@ const client = new MongoClient(uri, {
   },
 });
 
+// Verify User MiddleWare
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const serviceCollection = client.db('cleanCoDB').collection('services');
-    const bookingCollection = client.db('cleanCoDB').collection('bookings');
+    const foodCollection = client.db('kolaDB').collection('foods');
+
+    // foods API
+    app.get('/api/v1/foods', async (req, res) => {
+      const { quantity, sortField, sortOrder, foodName } = req.query;
+
+      // Parse quantity as a number; if not provided, set it to null
+      const quantityLimit = quantity ? parseInt(quantity, 10) : null;
+
+      let queryObj = {};
+      let sortObj = {};
+
+      // Add a query for foodName if provided
+      if (foodName) {
+        queryObj.foodName = { $regex: new RegExp(foodName, 'i') };
+      }
+
+      // pagination
+      const page = Number(req.query.page);
+      const limit = Number(req.query.limit);
+      const skip = (page - 1) * limit;
+
+      // sorting
+      if (sortField && sortField === 'expiredDateTime' && sortOrder) {
+        sortObj[sortField] = sortOrder === 'desc' ? -1 : 1;
+      }
+
+      const cursor = foodCollection
+        .find(queryObj)
+        .skip(skip)
+        .limit(limit)
+        .sort(sortObj);
+
+      // Convert the cursor to an array
+      const result = await cursor.toArray();
+
+      if (quantityLimit !== null) {
+        // If a quantity limit is specified, return only the requested number of items
+        const limitedResult = result.slice(0, quantityLimit);
+        res.send(limitedResult);
+      } else {
+        // If no quantity limit is specified, return all items
+        res.send(result);
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db('admin').command({ ping: 1 });
@@ -49,7 +94,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get('/', async (req, res) => {
-  res.send('Kola Food Sharing server is running : 🆗');
+  res.send('Kola Food Sharing server is : 🆗');
 });
 
 app.listen(port, () => {
