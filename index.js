e00fe339d228c0e38da71f93e0e229dd9c8bd908
Sakebul-islam <mdsakebul66@gmail.com>
2023-11-08 15:29:16 +0600

    find specific foods on basis of userEmail featcher added

--- index.js
+++ index.js
@@ -38,45 +38,90 @@ async function run() {
     const foodCollection = client.db('kolaDB').collection('foods');
     const requestCollection = client.db('kolaDB').collection('request');
 
-    app.get('/api/v1/foods', async (req, res) => {
-      const { quantity, sortField, sortOrder, foodName } = req.query;
+    // app.get('/api/v1/foods', async (req, res) => {
+    //   const { quantity, sortField, sortOrder, foodName } = req.query;
 
-      const quantityLimit = quantity ? parseInt(quantity, 10) : null;
+    //   const quantityLimit = quantity ? parseInt(quantity, 10) : null;
 
-      let queryObj = {};
-      let sortObj = {};
+    //   let queryObj = {};
+    //   let sortObj = {};
 
-      if (foodName) {
-        queryObj.foodName = { $regex: new RegExp(foodName, 'i') };
-      }
+    //   if (foodName) {
+    //     queryObj.foodName = { $regex: new RegExp(foodName, 'i') };
+    //   }
 
-      const page = Number(req.query.page);
-      const limit = Number(req.query.limit);
-      const skip = (page - 1) * limit;
+    //   const page = Number(req.query.page);
+    //   const limit = Number(req.query.limit);
+    //   const skip = (page - 1) * limit;
 
-      // sorting
-      if (sortField && sortField === 'expiredDateTime' && sortOrder) {
-        sortObj[sortField] = sortOrder === 'desc' ? -1 : 1;
-      } else {
-        sortObj.foodQuantity = -1;
-      }
+    //   // sorting
+    //   if (sortField && sortField === 'expiredDateTime' && sortOrder) {
+    //     sortObj[sortField] = sortOrder === 'desc' ? -1 : 1;
+    //   } else {
+    //     sortObj.foodQuantity = -1;
+    //   }
 
-      const cursor = foodCollection
-        .find(queryObj)
-        .skip(skip)
-        .limit(limit)
-        .sort(sortObj);
+    //   const cursor = foodCollection
+    //     .find(queryObj)
+    //     .skip(skip)
+    //     .limit(limit)
+    //     .sort(sortObj);
 
-      // Convert the cursor to an array
-      const result = await cursor.toArray();
+    //   // Convert the cursor to an array
+    //   const result = await cursor.toArray();
 
-      if (quantityLimit !== null) {
-        const limitedResult = result.slice(0, quantityLimit);
-        res.send(limitedResult);
-      } else {
-        res.send(result);
-      }
-    });
+    //   if (quantityLimit !== null) {
+    //     const limitedResult = result.slice(0, quantityLimit);
+    //     res.send(limitedResult);
+    //   } else {
+    //     res.send(result);
+    //   }
+    // });
+
+app.get('/api/v1/foods', async (req, res) => {
+  const { quantity, sortField, sortOrder, foodName, userEmail } = req.query;
+
+  const quantityLimit = quantity ? parseInt(quantity, 10) : null;
+
+  let queryObj = {};
+  let sortObj = {};
+
+  if (foodName) {
+    queryObj.foodName = { $regex: new RegExp(foodName, 'i') };
+  }
+
+  if (userEmail) {
+    queryObj.donatorEmail = userEmail;
+  }
+
+  const page = Number(req.query.page);
+  const limit = Number(req.query.limit);
+  const skip = (page - 1) * limit;
+
+  // sorting
+  if (sortField && sortField === 'expiredDateTime' && sortOrder) {
+    sortObj[sortField] = sortOrder === 'desc' ? -1 : 1;
+  } else {
+    sortObj.foodQuantity = -1;
+  }
+
+  const cursor = foodCollection
+    .find(queryObj)
+    .skip(skip)
+    .limit(limit)
+    .sort(sortObj);
+
+  // Convert the cursor to an array
+  const result = await cursor.toArray();
+
+  if (quantityLimit !== null) {
+    const limitedResult = result.slice(0, quantityLimit);
+    res.send(limitedResult);
+  } else {
+    res.send(result);
+  }
+});
+
 
     app.get('/api/v1/foods/:id', async (req, res) => {
       const id = req.params.id;
